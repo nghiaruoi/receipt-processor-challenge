6d3618cab1113d8ccc2fea3519b5b1851e29d786
Nghia Le <nle0129@gmail.com>
2025-03-13 15:30:02 -0700

    First Commit

---
+++ src/main/java/com/spring/receiptprocessorchallenge/ReceiptProcessorChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.spring.receiptprocessorchallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReceiptProcessorChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReceiptProcessorChallengeApplication.class, args);
+	}
+
+}
